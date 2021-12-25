106020502edddead3428489aebf704bf7ff0bec0
365kim <365listener@gmail.com>
2021-12-25 16:34:22 +0900

    refactor: Section 내 사용하지 않는 프로퍼티 정리

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -2,14 +2,9 @@ import { SectionToScroll } from './styles';
 
 interface Props extends Partial<Omit<HTMLElement, 'children'>> {
   sectionHeight: number;
-  customTransform?: string;
   children: JSX.Element | JSX.Element[];
 }
 
-export const Section = ({ children, sectionHeight, customTransform }: Props) => {
-  return (
-    <SectionToScroll sectionHeight={sectionHeight} >
-      {children}
-    </SectionToScroll>
-  );
+export const Section = ({ children, sectionHeight }: Props) => {
+  return <SectionToScroll sectionHeight={sectionHeight}>{children}</SectionToScroll>;
 };
--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -2,12 +2,10 @@ import styled from 'styled-components';
 
 interface Props {
   sectionHeight: number;
-  customTransform?: string;
 }
 
 export const SectionToScroll = styled.section<Props>`
   position: relative;
   padding-top: 50vh;
   height: ${(props) => props.sectionHeight + 'px'};
-  
 `;
