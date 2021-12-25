c8608a575920d28526ce641b00a87735a87a048a
365kim <365listener@gmail.com>
2021-12-25 13:42:26 +0900

    feat: 라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
-import {
-  GlobalFooter,
-  GlobalNavBar,
-  LocalNavBar,
-  SectionDescription,
-  SectionHero,
-  SectionLast,
-  SectionPlain,
-} from './components';
-import { useResize, useScroll } from './hooks';
-import { SECTION } from './constants';
+import { Suspense } from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import { GlobalFooter, GlobalNavBar, LocalNavBar } from './components';
+import { Home } from './pages';
 
 export const App = () => {
-  const { sectionHeights, accSectionHeights, heightRatio } = useResize();
-  const { currentSection, isNavSticky } = useScroll(accSectionHeights);
-  const { HERO, PLAIN, DESCRIPTION, LAST } = SECTION;
-
   return (
-    <>
-      <header>
-        <GlobalNavBar />
-        <LocalNavBar isSticky={isNavSticky} />
-      </header>
-      <main>
-        <SectionHero
-          id="scroll-section-0"
-          sectionInfo={HERO}
-          isCurrentSection={currentSection === HERO.INDEX}
-          customHeight={sectionHeights[HERO.INDEX]}
-          heightRatio={heightRatio}
-        />
-        <SectionPlain
-          id="scroll-section-1"
-          sectionInfo={PLAIN}
-          isCurrentSection={currentSection === PLAIN.INDEX}
-          customHeight={sectionHeights[HERO.INDEX]}
-        />
-        <SectionDescription
-          id="scroll-section-2"
-          sectionInfo={DESCRIPTION}
-          isCurrentSection={currentSection === DESCRIPTION.INDEX}
-          customHeight={sectionHeights[DESCRIPTION.INDEX]}
-          heightRatio={heightRatio}
-        />
-        <SectionLast
-          id="scroll-section-3"
-          sectionInfo={LAST}
-          isCurrentSection={currentSection === LAST.INDEX}
-          customHeight={sectionHeights[LAST.INDEX]}
-        />
-      </main>
-      <GlobalFooter />
-    </>
+    <BrowserRouter>
+      <Suspense fallback="">
+        <header>
+          <GlobalNavBar />
+          <LocalNavBar />
+        </header>
+        <main>
+          <Routes>
+            <Route path={'/'} element={<Home />}></Route>
+          </Routes>
+        </main>
+        <GlobalFooter />
+      </Suspense>
+    </BrowserRouter>
   );
 };
