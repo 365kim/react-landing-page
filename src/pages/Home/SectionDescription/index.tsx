2e0fc0cd441e6a89fb3d39c30f33ee29c6b9a672
365kim <365listener@gmail.com>
2021-12-30 17:19:31 +0900

    feat: SectionDescription draw 함수 정의

--- src/pages/Home/SectionDescription/index.tsx
+++ src/pages/Home/SectionDescription/index.tsx
@@ -5,6 +5,19 @@ import { messagesChildren } from './messages';
 import { CANVAS_HEIGHT, CANVAS_WIDTH, SECTION_DESCRIPTION } from '../../../constants';
 
 const { VIDEO_CONFIG, MESSAGES_CONFIG } = SECTION_DESCRIPTION;
+const VIDEO_IMAGE_COUNT = 960;
+
+const nthImagePath = (i: number) => `./video/001/IMG_${7027 + i}.JPG`;
+const imageElements = Array(VIDEO_IMAGE_COUNT).map((_, i) => {
+  const $image = new Image();
+
+  $image.src = nthImagePath(i);
+  return $image;
+});
+
+const draw = (context: CanvasRenderingContext2D, imageIndex = 0, dx = 0, dy = 0) => {
+  context.drawImage(imageElements[imageIndex], dx, dy);
+};
 
 export const SectionDescription = ({ isCurrentSection, sectionHeight, sectionScrollY, heightRatio }: Props) => {
   const scrollRatio = toFixed(sectionScrollY / sectionHeight);
@@ -17,6 +30,7 @@ export const SectionDescription = ({ isCurrentSection, sectionHeight, sectionScr
         scale={heightRatio}
         width={CANVAS_WIDTH}
         height={CANVAS_HEIGHT}
+        draw={draw}
       ></StickyCanvas>
       <>
         {messagesChildren.map((children, index) => {
